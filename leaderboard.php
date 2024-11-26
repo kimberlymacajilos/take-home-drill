bf102401b440eea60ea84355172b14d053c07a7e
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2024-11-26 17:36:27 +0800

    added filter date feature form

--- leaderboard.php
+++ leaderboard.php
@@ -8,9 +8,17 @@ include "connection.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Leaderboard</title>
 </head>
 <body>
-    
+    <h1>Leaderboard</h1>
+
+    <form method="GET" action="">
+        <label for="startdate">Start Date:</label>
+        <input type="date" id="start" name="start" required>
+        <label for="enddate">End Date:</label>
+        <input type="date" id="end" name="end" required>
+        <button type="submit">Filter</button>
+    </form>
 </body>
 </html>
